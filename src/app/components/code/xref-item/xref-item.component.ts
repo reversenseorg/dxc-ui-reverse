66bad4b3ea26cd3c805c54405857eaf8c4437430
georges.michel <georges@reversense.com>
2024-07-11 03:31:50 +0200

    Add a component to represent a single interactive xref

---
+++ src/app/components/code/xref-item/xref-item.component.ts
@@ -0,0 +1,101 @@
+import {
+    AfterViewInit,
+    ChangeDetectionStrategy,
+    ChangeDetectorRef,
+    Component,
+    EventEmitter,
+    Input,
+    OnInit,
+    Output
+} from '@angular/core';
+import {GLOBAL_ICONS} from "../../../cmp/GLOBAL_ICONS";
+import {Nullable} from "../../../base/Nullable";
+import {OutputService} from "../../output/ctrl/output.service";
+import {Tag} from "../../../models/tags/Tag";
+import {IconModel, IconModelCollection} from "../../../base/icon/IconModel";
+import {CODE_ICONS} from "../icons";
+
+let ctr = 0;
+
+
+export interface TargetApp {
+    method: "upload"|"store"|"download";
+
+}
+
+
+
+
+
+@Component({
+    selector: 'dxs-xref-item',
+    template: `
+    <div class="row g-0">
+        <div [ngClass]="'col-lg-'+((item.tags!=null&& item.tags.length>0)?'10':'12')">
+            <dxc-hook-status *ngIf="hookstatus" [item]="item"></dxc-hook-status>
+            <dxc-icon [model]="xrefIcon"></dxc-icon>
+            <dxc-node-token [interactive]="true" [item]="item"></dxc-node-token>
+        </div>
+        <div  *ngIf="item.tags!=null && item.tags.length>0" class="col-lg-2">
+            <ng-container *ngFor="let t of item.tags">
+                <dxc-tag-badge [editable]="true" [tagUUID]="t"></dxc-tag-badge>
+            </ng-container>
+        </div>
+    </div>
+    `,
+    styles:[`
+      .dxc-grid-body div.row {
+        &:hover {
+          background-color: var(--nav-btn-hover-bg);
+          color: var(--nav-btn-hover-color);
+        }
+        
+        &.focus {
+          background-color: var(--menu-bg-hover);
+          color: var(--text-100);
+        }
+        
+        &.footer {
+          position: absolute;
+          bottom: 0;
+          right: 0;
+        }
+        
+        cursor: pointer;
+      }
+    `]
+})
+export class XrefItemComponent implements OnInit, AfterViewInit {
+
+    @Input() item?:any; //ModelMethod|ModelClass|ModelField;
+
+    @Input() hookstatus = false;
+    @Input() type:"to"|"from";
+
+
+    xrefIcon:IconModel = CODE_ICONS.XREF_FROM;
+
+
+    selected:Nullable<Tag> = null;
+
+    canDrop = false;
+
+     icons:IconModelCollection = CODE_ICONS;
+     gIcons:IconModelCollection = GLOBAL_ICONS;
+
+    constructor(
+
+        private changeRef:ChangeDetectorRef) {
+
+    }
+
+    ngAfterViewInit() {
+        this.xrefIcon = (this.type=="to")? CODE_ICONS.XREF_TO : CODE_ICONS.XREF_FROM;
+    }
+
+    ngOnInit() {
+
+
+    }
+
+}
