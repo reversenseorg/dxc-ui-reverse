ef3a066d391879f6118140b724cfe3b51c173020
georges.michel <georges@reversense.com>
2024-07-11 03:29:09 +0200

    Add Hook status component

---
+++ src/app/components/hooks/hook-status/hook-status.component.ts
@@ -0,0 +1,139 @@
+import {
+    AfterViewInit,
+    ChangeDetectionStrategy,
+    ChangeDetectorRef,
+    Component,
+    EventEmitter,
+    Input,
+    OnInit,
+    Output
+} from '@angular/core';
+import {OutputService} from "../../output/ctrl/output.service";
+import ModelMethod from "../../../models/ModelMethod";
+import {HOOK_ICONS} from "../icons";
+import {NgbTooltipConfig} from "@ng-bootstrap/ng-bootstrap";
+import {ModelFunction} from "../../../models/ModelFunction";
+import {AbstractHook} from "../../../models/AbstractHook";
+import {HookService} from "../ctrl/hook.service";
+import {CodeControllerService} from "../../code/ctrl/code-controller.service";
+import {IconModel} from "../../../base/icon/IconModel";
+import {GLOBAL_ICONS} from "../../../cmp/GLOBAL_ICONS";
+import {NodeInternalType} from "../../../models/NodeInternalType";
+
+let ctr = 0;
+
+
+export interface TargetApp {
+    method: "upload"|"store"|"download";
+
+}
+
+
+
+
+
+@Component({
+    selector: 'dxc-hook-status',
+    template: `
+        <dxc-icon [ngbTooltip]="getTooltip()" [model]="icon" (click)="changeStatus()"></dxc-icon>
+    `,
+    styleUrls: ['../explorer-hooks/explorer-hooks.component.scss'],
+    providers: [NgbTooltipConfig],
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class HookStatusComponent implements OnInit {
+
+
+    @Input() item:ModelMethod|ModelFunction;
+    @Input() hook:AbstractHook;
+
+    icon:IconModel = GLOBAL_ICONS.SPINNER;
+
+    constructor(
+        private hookSvc: HookService,
+        private changeRef:ChangeDetectorRef) {
+
+        this.hookSvc.onHookEdit.subscribe((pHookEvent)=>{
+
+            if(this.hook!=null && this.hook.id==pHookEvent.hookID){
+
+                if(pHookEvent.hook!=null){
+                    this.hook = pHookEvent.hook;
+                    this.refreshHook();
+                }
+
+            }
+        })
+    }
+
+    refreshHookStatus():void {
+        console.log("refreshHookStatus > ",this.hook, this.hook.isEnable())
+        this.icon = this.hook.isEnable()? HOOK_ICONS.UP : HOOK_ICONS.DOWN;
+        this.changeRef.detectChanges();
+    }
+
+    refreshHook(){
+        if(this.hook != null){
+            this.refreshHookStatus();
+        }else if(this.item != null){
+            this.hookSvc.getHooksFor(this.item).subscribe((vHook)=>{
+
+                if(vHook.length==0){
+                    this.icon = HOOK_ICONS.NOT_HOOKED;
+                    this.changeRef.detectChanges();
+                }else{
+
+                    const h:AbstractHook[] = [];
+                    vHook.map( (vRaw:any)=>{
+                        const o:any = new AbstractHook(vRaw);
+
+                        o._t = "h";
+                        if(o.__==NodeInternalType.HOOK_NATIVE){
+                            o.symbol = o.func.substr(o.func.lastIndexOf(':')+1);
+                        }
+                        h.push( o as AbstractHook);
+                    });
+
+                    this.hook = h[0];
+                    this.refreshHook();
+                }
+
+            });
+        }else{
+            this.icon = HOOK_ICONS.UNKNOWN_HOOK_STATE;
+            this.changeRef.detectChanges();
+        }
+    }
+
+    ngOnInit() {
+        this.refreshHook();
+    }
+
+
+    /**
+     * To get tooltip text from hook status
+     *
+     * @return {string}
+     * @method
+     */
+    getTooltip(){
+        if(this.hook==null) return "";
+
+        if(this.hook.isEnable()){
+            return "Click to disable";
+        }else{
+            return "Click to enable";
+        }
+    }
+
+    changeStatus():void {
+        if(this.hook != null){
+            this.hookSvc.enableHook(this.hook, this.hook.isEnable()?false:true)
+                .subscribe(()=>{
+                    // nothing to do
+                });
+        }else{
+            // generate hook
+        }
+    }
+}
