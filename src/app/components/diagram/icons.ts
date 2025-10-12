10bcd7502b9177d52e10ed1d5e4daff3c149bed5
georges.michel <georges@reversense.com>
2025-10-12 02:26:25 +0200

    Fix diagram

---
+++ src/app/components/diagram/icons.ts
@@ -0,0 +1,11 @@
+import { IconModel} from "../../base/icon/IconModel";
+
+
+export const DIAGR_ICONS = {
+    EXPAND: new IconModel({ type: 'fas', name: 'expand-arrows-alt', color1: 'dxc-text-clear100' }),
+    HOME: new IconModel({ type: 'fas', name: 'home', color1: 'dxc-text-clear100' }),
+    PLUS: new IconModel({ type: 'fas', name: 'plus', color1: 'dxc-text-clear100' }),
+    TRASH: new IconModel({ type: 'fas', name: 'trash', color1: 'dxc-text-clear100' }),
+    SEARCH: new IconModel({ type: 'fas', name: 'search', color1: 'dxc-text-clear100' }),
+    TIMES: new IconModel({ type: 'fas', name: 'times', color1: 'dxc-text-clear100' }),
+};
