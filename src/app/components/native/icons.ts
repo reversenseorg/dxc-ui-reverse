7e6bf75f83995f0f41d55d556144743c455cca71
georges.michel <georges@reversense.com>
2025-11-27 12:29:44 +0100

    Add icon for native analysis : exported fucntionsm syscalls, decompiled code

--- src/app/components/native/icons.ts
+++ src/app/components/native/icons.ts
@@ -19,4 +19,22 @@ export const NATIVE_ICONS = {
     name: 'file-import',
     color1: 'dxc-text-clear100'
   }),
+    EXPORTS: new IconModel({
+        iconType: ICON_TYPE.ICON,
+        type: 'fal',
+        name: 'file-export',
+        color1: 'dxc-text-clear100'
+    }),
+    SYSCALL: new IconModel({
+        iconType: ICON_TYPE.ICON,
+        type: 'fal',
+        name: 'stethoscope',
+        color1: 'dxc-text-clear100'
+    }),
+    DECOMPILED: new IconModel({
+        iconType: ICON_TYPE.ICON,
+        type: 'fal',
+        name: 'code',
+        color1: 'dxc-text-clear100'
+    }),
 };
