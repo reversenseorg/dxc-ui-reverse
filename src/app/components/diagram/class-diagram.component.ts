d07d25c6286526e1e013294ea1f8d516a785fd9c
georges.michel <georges@reversense.com>
2025-11-27 12:28:47 +0100

    Add MermaidJS and corresponding services and class diagram viewer

--- src/app/components/diagram/class-diagram.component.ts
+++ src/app/components/diagram/class-diagram.component.ts
@@ -17,6 +17,9 @@ import {GraphDataService} from "../graph/graph.service";
 import {DiagramService} from "./diagram.service";
 import ModelMethod from "../../models/ModelMethod";
 import ModelField from "../../models/ModelField";
+import {Nullable} from "../../base/Nullable";
+import {CodeControllerService} from "../code/ctrl/code-controller.service";
+import {forkJoin} from "rxjs";
 
 
 export enum ClassStereotype {
@@ -31,7 +34,31 @@ export enum ClassStereotype {
     template: `
         <div #diagramContainer class="mermaid-container"></div>
     `,
-    styleUrls: ['./mermaid.scss']
+    styles: [`
+
+      /*.mermaid-container {
+        background-color: #ddd;
+      }
+      #diagram-id .dependency,
+      #diagram-id .relation {
+        stroke: #fff !important;
+      }
+      
+      .marker .relation {
+        stroke: #fff !important;
+      }
+
+      .marker .extension,
+      .marker .lollipop,
+      .marker .aggregation {
+        stroke: #fff !important;
+      }
+      .marker .composition,
+      .marker .dependency {
+        stroke: #fff !important;
+        fill: #fff !important;
+      }*/
+    `]
 })
 export class ClassDiagramComponent implements  AfterViewInit, OnDestroy {
 
@@ -40,6 +67,8 @@ export class ClassDiagramComponent implements  AfterViewInit, OnDestroy {
     @Input() classes:ModelClass[] =  [];
     // Icons pour la toolbar
     icons = DIAGR_ICONS;
+    styles:Record<string, string> = {};
+
 
     constructor(private diagSvc: DiagramService) {
 
@@ -48,6 +77,8 @@ export class ClassDiagramComponent implements  AfterViewInit, OnDestroy {
 
     ngAfterViewInit() {
         this.initializeMermaid();
+
+
     }
 
     private initializeMermaid() {
@@ -61,11 +92,57 @@ export class ClassDiagramComponent implements  AfterViewInit, OnDestroy {
             }
         });
 
+
+        this._setClassStyle(
+            `default`,
+            "fill:#777,stroke:#fff,color:#fff,stroke-width:2px"
+        );
+
+        let cls:any = {};
+        let needs
+
+        /*
+        this.classes.map(c => {
+            if(c.name==null) return;
+
+            if(cls[c.name]==null){
+                cls[c.name] = this.diagSvc.getClass(c.name);
+            }
+
+            if(c.implements!=undefined){
+                for(let i=0; i<c.implements.length; i++) {
+
+                }
+            }
+
+
+            if(c.supers!=undefined){
+                for(let i=0; i<c.supers.length; i++) {
+
+                }
+            }
+
+        });*/
+
+        /*forkJoin(needs).subscribe({
+            next: (vForkRes:any)=>{
+                console.log("Report > fork join > SUCCESS ",vForkRes);
+                this._changeRef.detectChanges();
+            },
+            error: (vErr)=>{
+                console.log("Report > fork join > ERROR : ",vErr);
+            }
+        })*/
+
         this._renderDiagram();
     }
 
     ngOnDestroy() {
 
+    }
+
+    private _setClassStyle(pClass:string, pStyle:string):void {
+        this.styles[pClass] = pStyle;
     }
 
     private _generateStereotype( pStereotype:ClassStereotype):string {
@@ -79,24 +156,76 @@ export class ClassDiagramComponent implements  AfterViewInit, OnDestroy {
         }
     }
 
-    private _generateClassDef(pClass:ModelClass, pStereotype = ClassStereotype.NONE){
+    private _encodeSymbol( pSymbol:string ):string {
+        let encoded = pSymbol;
+
+        /*
+        while(encoded.indexOf('<')>-1)
+            encoded = encoded.replace('<', '&lt');
+        while(encoded.indexOf('>')>-1)
+            encoded = encoded.replace('>', '&gt');
+        */
+        while(encoded.indexOf('<')>-1)
+            encoded = encoded.replace('<', '');
+        while(encoded.indexOf('>')>-1)
+            encoded = encoded.replace('>', '');
+        while(encoded.indexOf('$')>-1)
+            encoded = encoded.replace('$', '.');
+
+        return encoded;
+
+    }
+
+
+    private _generateClassDef(pClass:ModelClass, pStereotype = ClassStereotype.NONE, pStyle = ""){
 
         console.log("generate class def",pClass);
         let code = "";
+        let cdef = `class ${this._encodeSymbol(pClass.name as string)}${pStyle.length>0?":::"+pStyle:""}`;
+        let all = `${cdef}\n`;
+
 
         if(pClass.methods!=null){
             for(let i=0; i<pClass.methods.length; i++){
-                code += `${pClass.simpleName} : ${pClass.methods[i].__callSignature__}\n`;
+                if(pClass.methods[i].__callSignature__!=null){
+                    code += `+${this._encodeSymbol(pClass.methods[i].__callSignature__ as string)}\n`;
+                }
+
             }
         }
 
         if(pClass.fields){
             for(let i=0; i<pClass.fields.length; i++){
-                code += `${pClass.simpleName} : ${pClass.fields[i].type.name} ${pClass.fields[i].name}\n`;
+                code += `+${this._encodeSymbol(pClass.fields[i].type.name)} ${this._encodeSymbol(pClass.fields[i].name as string)}\n`;
             }
         }
 
-        return `class ${(pClass.simpleName!=null ? pClass.simpleName : pClass.name )}${this._generateStereotype(pStereotype)}${code.length>0?"{\n"+code+"}":";"}\n`;
+        const s = this._generateStereotype(pStereotype);
+        if(s.length>0){
+            all += `${s} ${pClass.simpleName!=null?pClass.simpleName:pClass.name}\n`;
+        }
+
+        if(code.length>0){
+            return all+`${cdef}{\n${code}\n}\n`;
+        }else{
+            return all+`\n`;
+        }
+    }
+
+    private _escapeString(pStr:string):string {
+        let str = pStr;
+        let match:any;
+
+        do{
+            str = str.replace(/([^\\])?"/,'$1\\"');
+            match = /^"|([^\\])"/.test(str);
+        }while(match);
+
+        return str;
+    }
+
+    private _addNote(pNote:string, pClass:Nullable<string> = null):string {
+        return `note ${pClass!=null?"for "+pClass:""}"${this._escapeString(pNote)}"`
     }
 
     private _generateMarkup():string {
@@ -105,44 +234,55 @@ export class ClassDiagramComponent implements  AfterViewInit, OnDestroy {
         let def:Record<string,string> = {};
         let inherit:string[] = [];
 
-        this.classes.forEach( (c)=>{
+        this.classes.forEach( (c,i)=>{
 
-            for(let i=0; i<c.implements.length; i++){
+            this._setClassStyle(
+                `focused${i}`,
+                "fill:#777,stroke:#76D93E,color:#fff,stroke-width:2px"
+            );
 
-                let intf = c.implements[i];
-                if(intf == null) continue;
+            if(c.implements!=undefined){
+                for(let i=0; i<c.implements.length; i++){
 
-                if(typeof intf!="string"){
-                    intf = (intf as ModelClass).name as string;
-                }
+                    let intf = c.implements[i];
+                    if(intf == null) continue;
 
-                if(def[intf]==null){
-                    def[intf] = this._generateClassDef(new ModelClass({
-                        name: intf
-                    }),ClassStereotype.INTERFACE);
-                }
+                    if(typeof intf!="string"){
+                        intf = (intf as ModelClass).name as string;
+                    }
 
-                code += `${intf} <.. ${c.simpleName}\n`;
-            }
+                    if(def[intf]==null){
+                        def[intf] = this._generateClassDef(new ModelClass({
+                            name: intf
+                        }),ClassStereotype.INTERFACE);
+                    }
 
-            for(let i=0; i<c.supers.length; i++){
-                let intf:ModelClass = c.supers[i] as ModelClass;
-                if(intf == null || intf.name==null) continue;
-
-                if(def[intf.name]==null){
-                    def[intf.name] = this._generateClassDef(
-                        intf,
-                        /*intf.isAbstract()? ClassStereotype.ABSTRACT : */ClassStereotype.NONE);
-                }
-
-                if(i==0){
-                    code += `${intf.name} <|-- ${c.name}\n`;
-                }else{
-                    code += `${c.supers[i-1].name} <|-- ${intf.name}\n`;
+                    code += `${intf} <.. ${this._encodeSymbol(c.name as string)}\n`;
                 }
             }
 
-            code += this._generateClassDef(c);
+
+
+            if(c.supers!=undefined){
+                for(let i=0; i<c.supers.length; i++){
+                    let intf:ModelClass = c.supers[i] as ModelClass;
+                    if(intf == null || intf.name==null) continue;
+
+                    if(def[intf.name]==null){
+                        def[intf.name] = this._generateClassDef(
+                            intf,
+                            /*intf.isAbstract()? ClassStereotype.ABSTRACT : */ClassStereotype.NONE);
+                    }
+
+                    if(i==0){
+                        code += `${this._encodeSymbol(intf.name)} <|-- ${this._encodeSymbol(c.name as string)}\n`;
+                    }else{
+                        code += `${this._encodeSymbol(intf.name)} <|-- ${this._encodeSymbol(c.supers[i-1].name as string)}\n`;
+                    }
+                }
+            }
+
+            code += this._generateClassDef(c,ClassStereotype.NONE,`focused${i}`);
             cls++;
         })
 
@@ -152,11 +292,52 @@ export class ClassDiagramComponent implements  AfterViewInit, OnDestroy {
         }else{
 
             for(let k in def){
-                code = def[k]+"\n"+code;
+                code = def[k]+code;
             }
 
-            console.log(`\nclassDiagram\n${code}\n`);
-            return `\nclassDiagram\n${code}\n`;
+            let styles = "";
+            for(let k in this.styles){
+                styles += `classDef ${k} ${this.styles[k]};\n`;
+            }
+
+            /*
+            config:
+  theme: 'default'
+  themeVariables:
+    primaryColor: '#BB2528'
+    primaryTextColor: '#fff'
+    primaryBorderColor: '#7C0000'
+    lineColor: '#F8B229'
+    secondaryColor: '#006100'
+    tertiaryColor: '#fff'
+---
+             */
+            console.log(`\nclassDiagram\n${code}\n${styles}\n`);
+            /*return `---
+config:
+  theme: 'default'
+  themeVariables:
+    primaryColor: '#999'
+    primaryTextColor: '#fff'
+    primaryBorderColor: '#fff'
+    lineColor: '#F8B229'
+    secondaryColor: '#999'
+    tertiaryColor: '#999'
+---
+classDiagram\n${code}\n${styles}\n`;*/
+
+            return `---
+config:
+  theme: 'default'
+  themeVariables:
+    primaryColor: '#999'
+    primaryTextColor: '#fff'
+    primaryBorderColor: '#fff'
+    lineColor: '#F8B229'
+    secondaryColor: '#999'
+    tertiaryColor: '#999'
+---
+classDiagram\n${code}\n${styles}\n`;
         }
     }
     private async _renderDiagram() {
--- src/app/components/diagram/diagram.service.ts
+++ src/app/components/diagram/diagram.service.ts
@@ -11,6 +11,17 @@ import {OutputService} from "../output/ctrl/output.service";
 import {CodeControllerService} from "../code/ctrl/code-controller.service";
 import ModelMethod from "../../models/ModelMethod";
 import ModelBasicBlock from "../../models/ModelBasicBlock";
+import {DxcApiToken} from "../../base/DxcApiToken";
+import ModelClass from "../../models/ModelClass";
+import {OutputMessage} from "../../cmp/OutputMessage";
+import {DexcaliburProjectUUID} from "../../models/DexcaliburProject";
+import {DxApiResponse} from "../../base/common/common";
+import {Nullable} from "../../base/Nullable";
+import ModelField from "../../models/ModelField";
+import {ModelFunction} from "../../models/ModelFunction";
+import ModelStringValue from "../../models/ModelStringValue";
+import ModelPackage from "../../models/ModelPackage";
+import ModelFile from "../../models/ModelFile";
 
 
 @Injectable({
@@ -57,8 +68,105 @@ export class DiagramService extends DxcApiService {
                 fetch: {method: 'POST', url: '/graph/fetch', format: 'json', auth: false, puid: true},
                 expand: {method: 'POST', url: '/graph/expand/:nodeId', format: 'json', auth: false, puid: true},
                 search: {method: 'POST', url: '/graph/search', format: 'json', auth: false, puid: true}
+            },
+            direct: {
+                // puid MUST be FALSE
+                disass: { method:'POST', url:'/code/direct/:puid/:nodetype/disass', format:'json', auth:false, puid:false },
+                search: { method:'POST', url:'/code/direct/:puid/:nodetype/search', format:'json', auth:false, puid:false },
+            },
+            code: {
+                fetch: {method: 'POST', url: '/graph/fetch', format: 'json', auth: false, puid: true},
+                cls: {method: 'GET', url: '/code/class/:id', format: 'json', auth:false /* removed */, puid: true}
             }
         }, http, outputSvc);
     }
 
+
+    retrieveNode<T>(pProjectUID: DexcaliburProjectUUID, pRef:INodeRef):Observable<DxApiResponse<Nullable<T>>> {
+        return this._process(
+            this.endpoints.direct.search,
+            {puid: pProjectUID, nodetype: pRef.__, nodeuid: pRef._uid}
+        ).pipe(
+            map(vRes => {
+
+                if (!vRes.success) {
+                    return {success: false, msg: (vRes.msg != null ? vRes.msg : ""), data: null};
+                }
+
+                return {
+                    success: vRes.success,
+                    msg: (vRes.msg != null ? vRes.msg : ""),
+                    data: (vRes.data != null ? this.createNodeFromRef<T>(pRef, vRes.data) : null)
+                };
+            })
+        );
+    }
+
+    getClass( pQuery:string, pDirect = false):Observable<any> {
+
+        if(pDirect){
+            const p = DxcApiToken.getInstance("puid");
+            return this.retrieveNode<ModelClass>(
+                (p!=null ? p.getToken() : ""),
+                {
+                    __: NodeInternalType.CLASS,
+                    _uid: pQuery
+                }
+            );
+        }else{
+            return this._process(
+                this.endpoints['code']['cls'],
+                {
+                    'id': pQuery
+                }
+            ).pipe(map((pRes:any)=>{
+                if(pRes.err){
+                    return null;
+                }else{
+                    return pRes;
+                }
+
+            }));
+        }
+
+    }
+
+    /**
+     *
+     * @param pRef
+     * @param pRaw
+     */
+    createNodeFromRef<T>(pRef:INodeRef, pRaw:any):Nullable<T> {
+
+        let node:any = null;
+        let type = (typeof pRef.__==="string"? parseInt(pRef.__,10):pRef.__);
+
+        switch(type){
+            case NodeInternalType.METHOD:
+                node = new ModelMethod(pRaw);
+                break;
+            case NodeInternalType.CLASS:
+                node = new ModelClass(pRaw);
+                break;
+            case NodeInternalType.FIELD:
+                node = new ModelField(pRaw);
+                break;
+            case NodeInternalType.FUNC:
+                node = new ModelFunction(pRaw);
+                break;
+            case NodeInternalType.STRING:
+                node = new ModelStringValue(pRaw);
+                break;
+            case NodeInternalType.PACKAGE:
+                node = new ModelPackage(pRaw);
+                break;
+            case NodeInternalType.FILE:
+                node = new ModelFile(pRaw);
+                break;
+        }
+
+
+
+        return node;
+    }
 }
